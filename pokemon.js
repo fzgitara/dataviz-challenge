d2fd3550374a0243e92de22a31707dc448dc0e19
fzgitara <fachrizalgita@gmail.com>
2018-04-30 17:38:30 +0700

    done

--- pokemon.js
+++ pokemon.js
@@ -22,19 +22,19 @@ d3.csv('pokemon.csv', function(d) {
   const pokemonGen1 = []
   
   data.map(pokemon => {
-    if(pokemon.Generation === 1){
+    if(pokemon.Generation === 1 && pokemon.Number <= 34){
       pokemonGen1.push(pokemon)
     }
   })
   console.log(pokemonGen1)
 
   const colorScale = d3.scaleLinear()
-    .domain([0, 85])
-    .range(['red', 'green'])
+    .domain([0, 130])
+    .range(['red', 'blue'])
   
   const svg = d3.select('.container').append('svg')
-                .attr('width', 1240)
-                .attr('height', 240)
+                .attr('width', 1400)
+                .attr('height', 500)
                 .style('background', 'azure')
   
   svg.selectAll('rect')
@@ -43,21 +43,62 @@ d3.csv('pokemon.csv', function(d) {
     .append('rect')
     .attr('class', 'bar')
     .attr('x', (d, i) => {
-      return i * 10
+      return i * 40
     })
     .attr('y', d => {
-      return 240 - d.HP
+      return 400 - d.Attack * 3
+    })
+    .on('mouseover', function (d, i) {
+      d3.select(this).style('fill', 'pink')
+    })
+    .on('mouseout', function (d, i) {
+      d3.select(this).style('fill', colorScale(d.Attack))
     })
     .transition()
-    .duration(50)
+    .duration(100)
     .delay((d, i) => {
-      return i * 50
+      return i * 100
     })
-    .attr('width', 10)
+    .attr('width', 35)
     .attr('height', d => {
-      return d.HP
+      return d.Attack * 3
     })
     .attr('fill', d => {
-      return colorScale(d.HP)
+      return colorScale(d.Attack)
+    })
+
+  
+  svg.selectAll('name')
+    .data(pokemonGen1)
+    .enter()
+    .append('text')
+    .attr('class', 'name')
+    .transition()
+    .duration(100)
+    .delay((d, i) => {
+      return i * 100
+    })
+    .text(d => {
+      return d.Name
+    })
+    .attr('transform', (d,i) => {
+      return `translate(${i * 40 + 10}, ${415}) rotate (45)`
+    })
+
+  svg.selectAll('attack')
+    .data(pokemonGen1)
+    .enter()
+    .append('text')
+    .attr('class', 'attack')
+    .transition()
+    .duration(100)
+    .delay((d, i) => {
+      return i * 100
+    })
+    .text(d => {
+      return d.Attack
+    })
+    .attr('transform', (d,i) => {
+      return `translate(${i * 40 + 7}, ${400 - d.Attack*3 - 5})`
     })
 })
